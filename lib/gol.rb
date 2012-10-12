009dc6a8b0c38a6aaa67c36325e8023b44824bb0
Matt Raibert <mattraibert@gmail.com>
2012-10-12 16:36:48 -0400

    Gol should be object

--- lib/gol.rb
+++ lib/gol.rb
@@ -1,11 +1,17 @@
-def format(world)
-  world.map { |stripe| stripe.map { |x| x == 1 ? ?0 : ' ' }.join }.join "\n"
-end
+class Gol
+  def initialize(world = init(10))
+    @world = world
+  end
 
-def neighborhood(world, x, y)
-  (-1..1).map { |dx| (-1..1).map { |dy| world[x + dx][y + dy] } }
-end
+  def format
+    @world.map { |stripe| stripe.map { |x| x == 1 ? ?0 : ' ' }.join }.join "\n"
+  end
 
-def init(size)
-  size.times.map { size.times.map { rand 2 }.freeze }.freeze
+  def neighborhood(x, y)
+    (-1..1).map { |dx| (-1..1).map { |dy| @world[x + dx][y + dy] } }
+  end
+
+  def self.init(size)
+    size.times.map { size.times.map { rand 2 }.freeze }.freeze
+  end
 end
--- test/gol_test.rb
+++ test/gol_test.rb
@@ -6,12 +6,14 @@ class GolTest < MiniTest::Unit::TestCase
            [0, 1, 0],
            [1, 1, 0]]
 
+  GOL = Gol.new(WORLD)
+
   def test_format
-    assert_equal "00 \n 0 \n00 ", format(WORLD)
+    assert_equal "00 \n 0 \n00 ", GOL.format
   end
 
   def test_neighborhood
-    assert_equal WORLD, neighborhood(WORLD, 1, 1)
-    assert_equal [[0, 1, 1], [0, 1, 1], [0, 0, 1]], neighborhood(WORLD, 0, 0)
+    assert_equal WORLD, GOL.neighborhood(1, 1)
+    assert_equal [[0, 1, 1], [0, 1, 1], [0, 0, 1]], GOL.neighborhood(0, 0)
   end
 end
