1ed110be335ec24b628b7344702b6b402523f631
Matt Raibert <mattraibert@gmail.com>
2012-10-20 10:27:35 -0400

    make new fast gol quack like the old one

--- lib/gol/fast_gol.rb
+++ lib/gol/fast_gol.rb
@@ -2,16 +2,18 @@ require 'five_leaves/range_util.rb'
 require 'benchmark'
 
 class Field
+  attr_reader :data
+
   def initialize(field = Field.init(40, 150))
-    @field = field
+    @data = field
   end
 
   def rows
-    @rows ||= @field.size
+    @rows ||= @data.size
   end
 
   def columns
-    @columns ||= @field.first.size
+    @columns ||= @data.first.size
   end
 
   def self.init(rows, cols, density = 0.5)
@@ -19,7 +21,7 @@ class Field
   end
 
   def tick
-    @field = @field.each_with_index.map do |stripe, row|
+    @data = @data.each_with_index.map do |stripe, row|
       stripe.each_with_index.map do |it, col|
         descendant?(row, col)
       end
@@ -29,7 +31,7 @@ class Field
   def alive?(row, col)
     return false if row < 0 || row >= rows
     return false if col < 0 || col >= columns
-    @field[row][col]
+    @data[row][col]
   end
 
   def relatives(row, col)
@@ -44,7 +46,7 @@ class Field
 
   def descendant?(row, col)
     relatives = relatives(row, col)
-    if @field[row][col]
+    if @data[row][col]
       relatives == 3 || relatives == 4
     else
       relatives == 3
@@ -52,7 +54,11 @@ class Field
   end
 
   def format
-    @field.map { |row| row.map { |col| col ? ?0 : ' ' }.join }.join "\n"
+    @data.map { |row| row.map { |col| col ? ?0 : ' ' }.join }.join "\n"
+  end
+
+  def point(row, col)
+    @data[row][col]
   end
 end
 
